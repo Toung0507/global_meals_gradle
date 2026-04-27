d5364ef6d89394a86625fac48454a905c1e6bb16
kaoying <a0982686423@gmail.com>
2026-04-27 17:43:13 +0800

    新增付款狀態參數，設為ENUM

---
+++ src/main/java/com/example/global_meals_gradle/constants/PayStatus.java
@@ -0,0 +1,27 @@
+package com.example.global_meals_gradle.constants;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+//讓 Jackson 反序列化時忽略大小寫
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+public enum PayStatus {
+
+	UNPAID("UNPAID"), // 未付款
+	PAID("PAID"), // 已付款
+	REFUNDED("REFUNDED"); // 退款
+
+	private String payStatus;
+
+	private PayStatus(String payStatus) {
+		this.payStatus = payStatus;
+	}
+
+	public String getPayStatus() {
+		return payStatus;
+	}
+
+	public void setPayStatus(String payStatus) {
+		this.payStatus = payStatus;
+	}
+
+}
