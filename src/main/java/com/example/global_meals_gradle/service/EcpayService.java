01149e40e2ba4ffdecc5e46e32accd4cc30fac21
kaoying <a0982686423@gmail.com>
2026-05-05 21:51:42 +0800

    加上註解 暫時不會用到

--- src/main/java/com/example/global_meals_gradle/service/EcpayService.java
+++ src/main/java/com/example/global_meals_gradle/service/EcpayService.java
@@ -11,6 +11,7 @@ import com.example.global_meals_gradle.dao.OrdersDao;
 import com.example.global_meals_gradle.entity.Orders;
 import com.example.global_meals_gradle.utils.EcpayUtils;
 
+//暫時不會用到
 @Service
 public class EcpayService {
 
